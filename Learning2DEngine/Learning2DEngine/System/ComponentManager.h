90d6f9bdb90a800cb495bc33c38565e0f843aa6b
Xardas0327 <32038221+Xardas0327@users.noreply.github.com>
2025-01-21 17:26:43 +0000

    Add LateRendererComponent

--- Learning2DEngine/Learning2DEngine/System/ComponentManager.h
+++ Learning2DEngine/Learning2DEngine/System/ComponentManager.h
@@ -16,9 +16,10 @@ namespace Learning2DEngine
             friend class Singleton<ComponentManager>;
         private:
 			Render::RendererComponentHandler rendererComponentHandler;
+            Render::RendererComponentHandler lateRendererComponentHandler;
 
             ComponentManager()
-                : rendererComponentHandler()
+				: rendererComponentHandler(), lateRendererComponentHandler()
             {
 
             }
@@ -27,6 +28,8 @@ namespace Learning2DEngine
             void Add(const UpdaterComponent* component) {};
             // TODO: We will define it later
             void Remove(const UpdaterComponent* component) {};
+
+            //Render
 
             inline void AddToRenderer(Render::BaseRendererComponent* component)
             {
@@ -48,9 +51,32 @@ namespace Learning2DEngine
 				rendererComponentHandler.DoWithAllComponents();
             }
 
+            //LateRender
+
+            inline void AddToLateRenderer(Render::BaseRendererComponent* component)
+            {
+                lateRendererComponentHandler.Add(component);
+            }
+
+            inline void RemoveFromLateRenderer(Render::BaseRendererComponent* component)
+            {
+                lateRendererComponentHandler.Remove(component);
+            }
+
+            inline void NeedReorderLateRenderers()
+            {
+                lateRendererComponentHandler.NeedReorder();
+            }
+
+            inline void LateRender()
+            {
+                lateRendererComponentHandler.DoWithAllComponents();
+            }
+
             void Clear()
             {
 				rendererComponentHandler.Clear();
+				lateRendererComponentHandler.Clear();
             }
         };
     }
