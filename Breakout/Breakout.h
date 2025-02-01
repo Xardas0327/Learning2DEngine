809045f5d02be6d1c894c184325945dc4ccf579e
Xardas0327 <32038221+Xardas0327@users.noreply.github.com>
2025-02-01 16:57:50 +0000

    Add gamecontroller

--- Breakout/Breakout.h
+++ Breakout/Breakout.h
@@ -1,81 +1,21 @@
 #pragma once
 
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
 #include <irrklang/irrKlang.h>
 
 #include <Learning2DEngine/System/Game.h>
-#include <Learning2DEngine/System/GameObject.h>
-#include <Learning2DEngine/UI/Text2DLateRenderer.h>
-#include <Learning2DEngine/Physics/BoxColliderComponent.h>
-#include <Learning2DEngine/Physics/CircleColliderComponent.h>
+#include <Learning2DEngine/UI/FontSizePair.h>
 
-#include "Script/GameLevel.h"
-#include "Script/BackgroundController.h"
-#include "Script/PlayerController.h"
-#include "Script/BallController.h"
+#include "Script/GameController.h"
 #include "Script/PostProcessData.h"
-#include "Script/PowerUpController.h"
-#include "Script/PowerUpType.h"
-#include "Script/PowerUpActivation.h"
-#include "Script/PowerUpActivationEventItem.h"
-#include "Script/BallHitPlayerAction.h"
-#include "Script/BallHitPlayerEventItem.h"
-#include "Script/BallHitBrickAction.h"
-#include "Script/BallHitBrickEventItem.h"
 
-enum GameState {
-    GAME_ACTIVE,
-    GAME_MENU,
-    GAME_WIN
-};
-
-class Breakout : public virtual Learning2DEngine::System::Game,
-                 public virtual PowerUpActivation,
-                 public virtual BallHitPlayerAction,
-                 public virtual BallHitBrickAction
+class Breakout : public virtual Learning2DEngine::System::Game
 {
 private:
-    GameState state;
-    std::vector<PowerUpController*> powerUps;
-    std::vector<GameLevel> levels;
-    unsigned int selectedLevel;
-    unsigned int lives;
-    BackgroundController* backgroundController;
-    PlayerController* playerController;
-    BallController* ballController;
-    irrklang::ISoundEngine* soundEngine;
     const Learning2DEngine::UI::FontSizePair fontSizePair;
+    irrklang::ISoundEngine* soundEngine;
     PostProcessData* postProcessData;
-    float shakeTime;
-    PowerUpActivationEventItem powerUpActivationEventItem;
-    BallHitPlayerEventItem ballHitPlayerEventItem;
-    BallHitBrickEventItem ballHitBrickEventItem;
-
-    Learning2DEngine::UI::Text2DLateRenderer* liveText;
-    Learning2DEngine::UI::Text2DLateRenderer* startText;
-    Learning2DEngine::UI::Text2DLateRenderer* levelSelectorText;
-    Learning2DEngine::UI::Text2DLateRenderer* winText;
-    Learning2DEngine::UI::Text2DLateRenderer* retryText;
-
-protected:
-    void InitSystem();
-    void InitObjects();
-    void Update() override;
-
-    void ProcessInput();
-    void ShakeScreen();
-    void IsLiveLost();
-    void IsLevelCompleted();
-
-    void ResetLevel();
-    void ResetPlayer();
-
-    void SpawnPowerUps(glm::vec2 position);
-    bool IsPowerUpActive(const PowerUpType& type);
-    void UpdatePowerUps();
-    void ClearPowerUps();
+    GameController* gameController;
 
 public:
     Breakout();
@@ -83,7 +23,4 @@ public:
 
     void Init() override;
     void Terminate() override;
-    void ActivatePowerUp(PowerUpType powerUp) override;
-    void BallHitPlayer() override;
-    void BallHitBrick(BrickController* brick) override;
 };
