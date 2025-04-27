7bf837c5e12d322f6d3b69211d3985312b8d588d
Xardas0327 <32038221+Xardas0327@users.noreply.github.com>
2025-04-27 13:36:28 +0100

    ADD DebugCircleColliderRenderComponent

---
+++ Learning2DEngine/Learning2DEngine/DebugTool/DebugCircleColliderRenderer.cpp
@@ -0,0 +1,144 @@
+#include "DebugCircleColliderRenderer.h"
+
+#include <glm/gtc/matrix_transform.hpp>
+
+#include "DebugRenderData.h"
+#include "DebugMacro.h"
+
+#include "../System/Game.h"
+#include "../System/ResourceManager.h"
+#include "../System/ComponentManager.h"
+#include "../Render/RenderManager.h"
+#include "../Render/ShaderConstant.h"
+#include "../Physics/BaseCircleColliderComponent.h"
+
+namespace Learning2DEngine
+{
+	using namespace System;
+	using namespace Render;
+	using namespace Physics;
+
+	namespace DebugTool
+	{
+		DebugCircleColliderRenderer::DebugCircleColliderRenderer()
+			: shader(), vao(0), vbo(0), renderData()
+		{
+
+		}
+
+		void DebugCircleColliderRenderer::InitShader()
+		{
+			auto& resourceManager = System::ResourceManager::GetInstance();
+			shader = resourceManager.IsShaderExist(ShaderConstant::SIMPLE_COLOR_NAME)
+				? resourceManager.GetShader(ShaderConstant::SIMPLE_COLOR_NAME)
+				: resourceManager.LoadShader(
+					ShaderConstant::SIMPLE_COLOR_NAME,
+					ShaderConstant::GetSimpleColorVertexShader(),
+					ShaderConstant::GetSimpleColorFragmentShader());
+		}
+
+		void DebugCircleColliderRenderer::InitVao()
+		{
+			float vertices[L2DE_DEBUG_CIRCLE_SEGMENT * 2];
+
+			for (int i = 0; i < L2DE_DEBUG_CIRCLE_SEGMENT; ++i) {
+				float theta = 2.0f * 3.14159265359f * i / L2DE_DEBUG_CIRCLE_SEGMENT;
+				vertices[i * 2] = glm::cos(theta);
+				vertices[i * 2 + 1] = glm::sin(theta);
+			}
+
+
+			glGenVertexArrays(1, &vao);
+			glGenBuffers(1, &vbo);
+
+			glBindVertexArray(vao);
+
+			glBindBuffer(GL_ARRAY_BUFFER, vbo);
+			glBufferData(GL_ARRAY_BUFFER, L2DE_DEBUG_CIRCLE_SEGMENT * 2 * sizeof(float), vertices, GL_STATIC_DRAW);
+
+			glEnableVertexAttribArray(0);
+			glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
+
+			glBindBuffer(GL_ARRAY_BUFFER, 0);
+			glBindVertexArray(0);
+		}
+
+		void DebugCircleColliderRenderer::Init()
+		{
+			if (ComponentManager::GetInstance().GetThreadSafe())
+			{
+				std::lock_guard<std::mutex> lock(RenderManager::GetInstance().mutex);
+				InitShader();
+				InitVao();
+			}
+			else
+			{
+				InitShader();
+				InitVao();
+			}
+		}
+
+		void DebugCircleColliderRenderer::DestroyObject()
+		{
+			glDeleteVertexArrays(1, &vao);
+			glDeleteBuffers(1, &vbo);
+
+			renderData.clear();
+		}
+
+		void DebugCircleColliderRenderer::Destroy()
+		{
+			if (ComponentManager::GetInstance().GetThreadSafe())
+			{
+				std::lock_guard<std::mutex> lock(RenderManager::GetInstance().mutex);
+				DestroyObject();
+			}
+			else
+			{
+				DestroyObject();
+			}
+		}
+
+		void DebugCircleColliderRenderer::SetData(const std::map<int, std::vector<Render::RenderData*>>& renderData)
+		{
+			this->renderData = renderData;
+		}
+
+		void DebugCircleColliderRenderer::Draw(int layer)
+		{
+			if (renderData.find(layer) == renderData.end())
+				return;
+
+			shader.Use();
+			shader.SetMatrix4("projection", Game::mainCamera.GetProjection());
+			shader.SetMatrix4("view", Game::mainCamera.GetViewMatrix());
+
+			glBindVertexArray(vao);
+
+			for (auto data : renderData[layer])
+			{
+				auto colliderData = static_cast<DebugRenderData<BaseCircleColliderComponent>*>(data);
+				if (!colliderData->objectComponent->isActive)
+					continue;
+
+				glm::mat4 model = glm::mat4(1.0f);
+
+				model = glm::translate(model,
+					glm::vec3(colliderData->objectComponent->GetColliderCenter() + colliderData->objectComponent->colliderOffset, 0.0f)
+				);
+				model = glm::scale(model, glm::vec3(colliderData->objectComponent->colliderRadius, colliderData->objectComponent->colliderRadius, 1.0f));
+
+				shader.SetMatrix4("model", model);
+				shader.SetVector4f("color",
+					colliderData->objectComponent->mode == ColliderMode::TRIGGER
+					? L2DE_DEBUG_SHOW_COLLIDER_TRIGGER_COLOR
+					: L2DE_DEBUG_SHOW_COLLIDER_COLOR
+				);
+
+				glDrawArrays(GL_LINE_LOOP, 0, L2DE_DEBUG_CIRCLE_SEGMENT);
+			}
+
+			glBindVertexArray(0);
+		}
+	}
+}
---
+++ Learning2DEngine/Learning2DEngine/DebugTool/DebugCircleColliderRenderer.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <vector>
+#include <map>
+
+#include <glad/glad.h>
+
+#include "../System/Singleton.h"
+#include "../Render/IRenderer.h"
+#include "../Render/Shader.h"
+#include "../Render/RenderData.h"
+
+namespace Learning2DEngine
+{
+	namespace DebugTool
+	{
+#define L2DE_DEBUG_CIRCLE_SEGMENT 50
+
+		class DebugCircleColliderRenderer : public Render::IRenderer, public virtual System::Singleton<DebugCircleColliderRenderer>
+		{
+			friend class Singleton<DebugCircleColliderRenderer>;
+		private:
+			Render::Shader shader;
+			GLuint vao;
+			GLuint vbo;
+			std::map<int, std::vector<Render::RenderData*>> renderData;
+
+			DebugCircleColliderRenderer();
+
+			void InitShader();
+			void InitVao();
+			void DestroyObject();
+		public:
+			void Init() override;
+			void Destroy() override;
+			void SetData(const std::map<int, std::vector<Render::RenderData*>>& renderData) override;
+			void Draw(int layer) override;
+		};
+	}
+}
