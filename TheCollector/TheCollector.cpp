e6cd203114d6391e8c78371935b0601d6fc99f2a
Xardas0327 <32038221+Xardas0327@users.noreply.github.com>
2025-05-08 15:41:38 +0100

    add BushController

--- TheCollector/TheCollector.cpp
+++ TheCollector/TheCollector.cpp
@@ -11,6 +11,7 @@
 #include "CoinController.h"
 #include "MovingPlatformController.h"
 #include "PlatformController.h"
+#include "BushController.h"
 #include "PlayerController.h"
 
 using namespace Learning2DEngine::Render;
@@ -38,6 +39,10 @@ void TheCollector::Init()
     resourceManager.LoadTextureFromFile("KnightLeft", "Assets/Images/KnightLeft.png", basicSettings);
     resourceManager.LoadTextureFromFile("Ground1", "Assets/Images/Ground1.png", basicSettings);
     resourceManager.LoadTextureFromFile("Ground2", "Assets/Images/Ground2.png", basicSettings);
+    resourceManager.LoadTextureFromFile("Bush1", "Assets/Images/Bush1.png", basicSettings);
+    resourceManager.LoadTextureFromFile("Bush2", "Assets/Images/Bush2.png", basicSettings);
+    resourceManager.LoadTextureFromFile("Bush3", "Assets/Images/Bush3.png", basicSettings);
+    resourceManager.LoadTextureFromFile("Bush4", "Assets/Images/Bush4.png", basicSettings);
 
     //Background color
     renderManager.SetClearColor(0.0f, 0.6f, 0.9f, 1.0f);
@@ -111,6 +116,16 @@ void TheCollector::Init()
     MovingPlatformController::Create(glm::vec2(500.0f, 150.0f), glm::vec2(500.0f, 500.0f), "LargePlatform");
 
     MovingPlatformController::Create(glm::vec2(950.0f, 450.0f), glm::vec2(1150.0f, 450.0f), "LargePlatform");
+
+    //Bush
+    BushController::Create(glm::vec2(500.0f, 600.0f), "Bush2", BUSH_SIZE, -1);
+    BushController::Create(glm::vec2(550.0f, 600.0f), "Bush1", BUSH_SIZE, 1);
+    BushController::Create(glm::vec2(600.0f, 600.0f), "Bush2", BUSH_SIZE, -1);
+    BushController::Create(glm::vec2(650.0f, 600.0f), "Bush1", BUSH_SIZE, 1);
+    BushController::Create(glm::vec2(700.0f, 600.0f), "Bush2", BUSH_SIZE, -1);
+    BushController::Create(glm::vec2(750.0f, 600.0f), "Bush1", BUSH_SIZE, 1);
+    BushController::Create(glm::vec2(1362.5f, 250.0f), "Bush3", BUSH_SIZE, -1);
+    BushController::Create(glm::vec2(1125.0f, 100.0f), "Bush4", BUSH_SIZE, 1);
 
     //Coin
     CoinController::Create(glm::vec2(50.0f, 75.0f));
