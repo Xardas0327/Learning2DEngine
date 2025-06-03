bb966cacf18bcef4b3b3baf1ddbc9674debc5137
Xardas0327 <32038221+Xardas0327@users.noreply.github.com>
2025-06-03 16:34:17 +0100

    Create GameObjectManager.cpp

--- Learning2DEngine/Learning2DEngine/System/GameObjectManager.h
+++ Learning2DEngine/Learning2DEngine/System/GameObjectManager.h
@@ -22,108 +22,27 @@ namespace Learning2DEngine
 
 			bool isThreadSafe;
 
-			GameObjectManager()
-				: gameObjects(), removableGameObjects(), addMutex(), removeMutex(), isThreadSafe(false)
-			{
-			}
+			GameObjectManager();
 		public:
 
-			GameObject* CreateGameObject(bool isActive = true)
-			{
-				auto gameObject = new GameObject(isActive);
-
-				if (isThreadSafe)
-				{
-					std::lock_guard<std::mutex> lock(addMutex);
-					gameObjects.push_back(gameObject);
-				}
-				else
-					gameObjects.push_back(gameObject);
-
-				return gameObject;
-			}
-
-			GameObject* CreateGameObject(const Transform& transform, bool isActive = true)
-			{
-				auto gameObject = new GameObject(transform, isActive);
-
-				if (isThreadSafe)
-				{
-					std::lock_guard<std::mutex> lock(addMutex);
-					gameObjects.push_back(gameObject);
-				}
-				else
-					gameObjects.push_back(gameObject);
-
-				return gameObject;
-			}
+			GameObject* CreateGameObject(bool isActive = true);
+			GameObject* CreateGameObject(const Transform& transform, bool isActive = true);
 
 			/// <summary>
 			/// The gameObject and its components will be destroyed.
 			/// It will be inactive immediately, but it will be destroyed just end of the frame only.
 			/// </summary>
-			void DestroyGameObject(GameObject* gameObject)
-			{
-				gameObject->isActive = false;
-				if (isThreadSafe)
-				{
-					std::lock_guard<std::mutex> lock(removeMutex);
-					removableGameObjects.push_back(gameObject);
-				}
-				else
-					removableGameObjects.push_back(gameObject);
-			}
+			void DestroyGameObject(GameObject* gameObject);
 
 			/// <summary>
 			/// The gameObject of component and its components will be destroyed.
 			/// It will be inactive immediately, but it will be destroyed just end of the frame only.
 			/// </summary>
-			void DestroyGameObject(Component* component)
-			{
-				component->gameObject->isActive = false;
-				if (isThreadSafe)
-				{
-					std::lock_guard<std::mutex> lock(removeMutex);
-					removableGameObjects.push_back(component->gameObject);
-				}
-				else
-					removableGameObjects.push_back(component->gameObject);
-			}
+			void DestroyGameObject(Component* component);
 
-			void DestroyMarkedGameObjects()
-			{
-				if (removableGameObjects.size() > 0)
-				{
-					auto newEnd = remove_if(gameObjects.begin(), gameObjects.end(),
-						[this](GameObject* gameObject)
-						{
-							auto it = std::find(removableGameObjects.begin(), removableGameObjects.end(), gameObject);
-							if (it != removableGameObjects.end())
-							{
-								gameObject->Destroy();
-								delete gameObject;
-								return true;
-							}
+			void DestroyMarkedGameObjects();
 
-							return false;
-						});
-					gameObjects.erase(newEnd, gameObjects.end());
-
-					removableGameObjects.clear();
-				}
-			}
-
-			void DestroyAllGameObjects()
-			{
-				for(GameObject* gameObject : gameObjects)
-				{
-					gameObject->Destroy();
-					delete gameObject;
-				}
-
-				gameObjects.clear();
-				removableGameObjects.clear();
-			}
+			void DestroyAllGameObjects();
 
 			inline void SetThreadSafe(bool value)
 			{
