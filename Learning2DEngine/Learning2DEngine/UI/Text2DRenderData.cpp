b4940c3b06771782652974c24c0dea2196516d08
Xardas0327 <32038221+Xardas0327@users.noreply.github.com>
2025-07-15 21:34:22 +0100

    Textbox and rewrite Text Rotation (#94)
    * Add basic TextBoxComponent
    
    * Add Padding to TextBoxComponent
    
    * MOD Text rotation
    
    * Fix padding
    
    * Optimize GetTextSize
    
    * Add TextBoxMode
    
    * Update Documentation

--- Learning2DEngine/Learning2DEngine/UI/Text2DRenderData.cpp
+++ Learning2DEngine/Learning2DEngine/UI/Text2DRenderData.cpp
@@ -9,8 +9,8 @@ namespace Learning2DEngine
     namespace UI
     {
         Text2DRenderData::Text2DRenderData(const System::Component* component, const FontSizePair& fontSizePair, glm::vec4 color)
-            : RenderData(component), fontSizePair(fontSizePair), text(""), isModified(true),
-            previousModelMatrix(), characterVertices(), color(color), isUseCameraView(false)
+            : RenderData(component), fontSizePair(fontSizePair), text(""), shouldRecalcSize(true), shouldRecalcVertices(true),
+            previousModelMatrix(), characterVertices(), textSize(), color(color), isUseCameraView(false)
         {
         }
 
@@ -20,8 +20,8 @@ namespace Learning2DEngine
             const std::string& text,
             glm::vec4 color
         )
-            : RenderData(component), fontSizePair(fontSizePair), text(text), isModified(true),
-            previousModelMatrix(), characterVertices(), color(color), isUseCameraView(false)
+            : RenderData(component), fontSizePair(fontSizePair), text(text), shouldRecalcSize(true), shouldRecalcVertices(true),
+            previousModelMatrix(), characterVertices(), textSize(), color(color), isUseCameraView(false)
         {
         }
 
@@ -32,15 +32,15 @@ namespace Learning2DEngine
             bool isUseCameraView,
             glm::vec4 color
         )
-            : RenderData(component), fontSizePair(fontSizePair), text(text), isModified(true),
-            previousModelMatrix(), characterVertices(), color(color), isUseCameraView(isUseCameraView)
+            : RenderData(component), fontSizePair(fontSizePair), text(text), shouldRecalcSize(true), shouldRecalcVertices(true),
+            previousModelMatrix(), characterVertices(), textSize(), color(color), isUseCameraView(isUseCameraView)
         {
         }
 
 
         glm::mat2 Text2DRenderData::GetRotationMatrix() const
         {
-            float radians = glm::radians(component->gameObject->transform.GetRotation());
+            float radians = -1.0f * glm::radians(component->gameObject->transform.GetRotation());
 
             return glm::mat2(
                 glm::cos(radians), -glm::sin(radians),
@@ -51,25 +51,47 @@ namespace Learning2DEngine
         void Text2DRenderData::SetText(const std::string& text)
         {
             this->text = text;
-            isModified = true;
+            shouldRecalcSize = true;
+            shouldRecalcVertices = true;
         }
 
         void Text2DRenderData::SetText(std::string&& text)
         {
             this->text = std::move(text);
-            isModified = true;
+            shouldRecalcSize = true;
+            shouldRecalcVertices = true;
         }
 
         void Text2DRenderData::SetFontSizePair(const FontSizePair& fontSizePair)
         {
             this->fontSizePair = fontSizePair;
-            isModified = true;
+            shouldRecalcSize = true;
+            shouldRecalcVertices = true;
+        }
+
+        glm::vec2 Text2DRenderData::CalculateTextSize() const
+        {
+            TextCharacterSet& textCharacterSet = TextCharacterSet::GetInstance();
+            textCharacterSet.Load(fontSizePair);
+            CharacterMap& characterMap = textCharacterSet[fontSizePair];
+
+            glm::vec2 size(0.0f, characterMap['H'].bearing.y * component->gameObject->transform.GetScale().y);
+            for (std::string::const_iterator c = text.begin(); c != text.end(); ++c)
+            {
+                const auto& ch = characterMap[*c];
+                size.x += (ch.advance >> 6) * component->gameObject->transform.GetScale().x;
+            }
+
+            return size;
         }
 
         std::map<char, std::vector<glm::mat4>> Text2DRenderData::CalculateCharacterVertices() const
         {
+            glm::vec2 textLength = GetTextSize();
+
             std::map<char, std::vector<glm::mat4>> textMap;
             glm::vec2 startPosition(component->gameObject->transform.GetPosition());
+            glm::vec2 middlePosition = startPosition + glm::vec2(textLength.x / 2.0f, textLength.y / 2);
             glm::mat2 rotationMatrix = GetRotationMatrix();
 
             TextCharacterSet& textCharacterSet = TextCharacterSet::GetInstance();
@@ -88,10 +110,12 @@ namespace Learning2DEngine
                 float chWidth = ch.size.x * component->gameObject->transform.GetScale().x;
                 float chHeight = ch.size.y * component->gameObject->transform.GetScale().y;
 
-                glm::vec2 v1 = startPosition + (rotationMatrix * glm::vec2(chPositionX + chWidth, chPositionY + chHeight));
-                glm::vec2 v2 = startPosition + (rotationMatrix * glm::vec2(chPositionX + chWidth, chPositionY));
-                glm::vec2 v3 = startPosition + (rotationMatrix * glm::vec2(chPositionX, chPositionY));
-                glm::vec2 v4 = startPosition + (rotationMatrix * glm::vec2(chPositionX, chPositionY + chHeight));
+                glm::vec2 relativePoint = (startPosition - middlePosition) + glm::vec2(chPositionX, chPositionY);
+
+                glm::vec2 v1 = middlePosition + rotationMatrix * (relativePoint + glm::vec2(chWidth, chHeight));
+                glm::vec2 v2 = middlePosition + rotationMatrix * (relativePoint + glm::vec2(chWidth, 0.0f));
+                glm::vec2 v3 = middlePosition + rotationMatrix * relativePoint;
+                glm::vec2 v4 = middlePosition + rotationMatrix * (relativePoint + glm::vec2(0.0f, chHeight));
 
                 textMap[*c].push_back(glm::mat4(
                     v1.x, v1.y, 1.0f, 1.0f,
@@ -101,7 +125,7 @@ namespace Learning2DEngine
                 ));
 
                 // Calculate next character position
-                startPosition += rotationMatrix *
+                startPosition +=
                     glm::vec2(
                         // bitshift by 6 to get value in pixels (1/64th times 2^6 = 64)
                         (ch.advance >> 6) * component->gameObject->transform.GetScale().x,
@@ -114,11 +138,14 @@ namespace Learning2DEngine
         const std::map<char, std::vector<glm::mat4>>& Text2DRenderData::GetCharacterVertices()
         {
             auto& actualModelMatrix = component->gameObject->transform.GetModelMatrix();
-            if (isModified || previousModelMatrix != actualModelMatrix)
+            if (shouldRecalcVertices || previousModelMatrix != actualModelMatrix)
             {
+                //Pre-calculation, because the CalculateCharacterVertices will call the const version.
+                GetTextSize();
+
                 characterVertices = CalculateCharacterVertices();
                 previousModelMatrix = actualModelMatrix;
-                isModified = false;
+                shouldRecalcVertices = false;
             }
 
             return characterVertices;
@@ -127,10 +154,28 @@ namespace Learning2DEngine
         std::map<char, std::vector<glm::mat4>> Text2DRenderData::GetCharacterVertices() const
         {
             auto& actualModelMatrix = component->gameObject->transform.GetModelMatrix();
-            if (isModified || previousModelMatrix != actualModelMatrix)
+            if (shouldRecalcVertices || previousModelMatrix != actualModelMatrix)
                 return CalculateCharacterVertices();
 
             return characterVertices;
+        }
+
+        glm::vec2 Text2DRenderData::GetTextSize()
+        {
+            if (shouldRecalcSize)
+            {
+                textSize = CalculateTextSize();
+                shouldRecalcSize = false;
+            }
+            return CalculateTextSize();
+        }
+
+        glm::vec2 Text2DRenderData::GetTextSize() const
+        {
+            if (shouldRecalcSize)
+                return CalculateTextSize();
+
+            return CalculateTextSize();
         }
     }
 }
---
+++ Learning2DEngine/Learning2DEngine/UI/TextBoxComponent.cpp
@@ -0,0 +1,171 @@
+#include "TextBoxComponent.h"
+
+#include "../System/GameObjectManager.h"
+
+namespace Learning2DEngine
+{
+	using namespace System;
+	using namespace Render;
+
+    namespace UI
+    {
+		TextBoxComponent::TextBoxComponent(
+			GameObject* gameObject,
+			SimpleText2DRenderComponent& textComponent,
+			TextBoxMode textBoxMode,
+			int layer,
+			glm::vec4 color)
+			: LateUpdaterComponent(gameObject), Component(gameObject),
+			  textRenderData(&textComponent.data), simpleRenderComponent(nullptr), multiRenderComponent(nullptr),
+			initLayer(layer), initColor(color), initRendererMode(textComponent.mode), textBoxMode(textBoxMode)
+		{
+
+		}
+
+		TextBoxComponent::TextBoxComponent(
+			GameObject* gameObject,
+			Text2DRenderComponent& textComponent,
+			TextBoxMode textBoxMode,
+			int layer,
+			glm::vec4 color)
+			: LateUpdaterComponent(gameObject), Component(gameObject),
+			textRenderData(&textComponent.data), simpleRenderComponent(nullptr), multiRenderComponent(nullptr),
+			initLayer(layer), initColor(color), initRendererMode(textComponent.mode), textBoxMode(textBoxMode)
+		{
+
+		}
+
+		void TextBoxComponent::Init()
+		{
+			LateUpdaterComponent::Init();
+
+			auto renderGameObject = GameObjectManager::GetInstance().CreateGameObject();
+			switch (textBoxMode)
+			{
+				case TextBoxMode::SIMPLE:
+					simpleRenderComponent = renderGameObject->AddComponent<SimpleSpriteRenderComponent>(
+						initRendererMode,
+						initLayer,
+						initColor
+					);
+					simpleRenderComponent->data.isUseCameraView = textRenderData->isUseCameraView;
+					break;
+				case TextBoxMode::MULTI:
+					multiRenderComponent = renderGameObject->AddComponent<SpriteRenderComponent>(
+						initRendererMode,
+						initLayer,
+						initColor
+					);
+					multiRenderComponent->data.isUseCameraView = textRenderData->isUseCameraView;
+					break;
+			}
+		}
+
+		void TextBoxComponent::Destroy()
+		{
+			LateUpdaterComponent::Destroy();
+
+			switch (textBoxMode)
+			{
+				case TextBoxMode::SIMPLE:
+					GameObjectManager::GetInstance().DestroyGameObject(simpleRenderComponent);
+					break;
+				case TextBoxMode::MULTI:
+					GameObjectManager::GetInstance().DestroyGameObject(multiRenderComponent);
+					break;
+			}
+		}
+
+		void TextBoxComponent::LateUpdate()
+		{
+			auto textLength = textRenderData->GetTextSize();
+			switch (textBoxMode)
+			{
+				case TextBoxMode::SIMPLE:
+					simpleRenderComponent->data.isUseCameraView = textRenderData->isUseCameraView;
+					simpleRenderComponent->gameObject->transform = Transform(
+						glm::vec2(
+							textRenderData->component->gameObject->transform.GetPosition().x - paddingLeftRight,
+							textRenderData->component->gameObject->transform.GetPosition().y - paddingTopBottom
+						),
+						glm::vec2(
+							textLength.x + 2.0f * paddingLeftRight,
+							textLength.y + 2.0f * paddingTopBottom
+						),
+						textRenderData->component->gameObject->transform.GetRotation()
+					);
+					break;
+				case TextBoxMode::MULTI:
+					multiRenderComponent->data.isUseCameraView = textRenderData->isUseCameraView;
+					multiRenderComponent->gameObject->transform = Transform(
+						glm::vec2(
+							textRenderData->component->gameObject->transform.GetPosition().x - paddingLeftRight,
+							textRenderData->component->gameObject->transform.GetPosition().y - paddingTopBottom
+						),
+						glm::vec2(
+							textLength.x + 2.0f * paddingLeftRight,
+							textLength.y + 2.0f * paddingTopBottom
+						),
+						textRenderData->component->gameObject->transform.GetRotation()
+					);
+					break;
+			}
+		}
+
+		void TextBoxComponent::SetPadding(float padding)
+		{
+			paddingTopBottom = padding;
+			paddingLeftRight = padding;
+		}
+
+		int TextBoxComponent::GetLayer() const
+		{
+			switch (textBoxMode)
+			{
+			case TextBoxMode::SIMPLE:
+				return simpleRenderComponent->GetLayer();
+			case TextBoxMode::MULTI:
+				return multiRenderComponent->GetLayer();
+			}
+
+			return 0;
+		}
+
+		void TextBoxComponent::SetLayer(int layer)
+		{
+			switch (textBoxMode)
+			{
+			case TextBoxMode::SIMPLE:
+				simpleRenderComponent->SetLayer(layer);
+				break;
+			case TextBoxMode::MULTI:
+				multiRenderComponent->SetLayer(layer);
+				break;
+			}
+		}
+
+		glm::vec4 TextBoxComponent::GetColor() const
+		{
+			switch (textBoxMode)
+			{
+			case TextBoxMode::SIMPLE:
+				return simpleRenderComponent->data.color;
+			case TextBoxMode::MULTI:
+				return multiRenderComponent->data.color;
+			}
+
+			return glm::vec4(1.0f);
+		}
+
+		void TextBoxComponent::SetColor(const glm::vec4& color)
+		{
+			switch (textBoxMode)
+			{
+			case TextBoxMode::SIMPLE:
+				simpleRenderComponent->data.color = color;
+			case TextBoxMode::MULTI:
+				multiRenderComponent->data.color = color;
+			}
+		}
+    }
+}
