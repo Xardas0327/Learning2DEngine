1bc487825aeb0a6beefc58f54d307fb3557b6f91
Xardas0327 <32038221+Xardas0327@users.noreply.github.com>
2025-10-15 18:20:10 +0100

    ADD LoadProperties and LoadTilesProperties tests

--- Learning2DEngineTest/Editor/TiledMapLoaderTest.cpp
+++ Learning2DEngineTest/Editor/TiledMapLoaderTest.cpp
@@ -34,8 +34,8 @@ namespace Learning2DEngine
 
 			TEST_METHOD(LoadMapAttributes)
 			{
-				auto doc = std::make_unique<rapidxml::xml_document<>>();
 				rapidxml::file<> xmlFile("TestData/Editor/MapTest.tmx");
+				auto doc = std::make_unique<rapidxml::xml_document<>>();
 				doc->parse<0>(xmlFile.data());
 
 				auto mapNode = doc->first_node(TiledMapNodeMap);
@@ -54,6 +54,83 @@ namespace Learning2DEngine
 				Assert::AreEqual(map.IsInfinite(), false);
 				Assert::IsTrue(CompareFloat::IsEqual(map.GetBackgroundColor(), TiledMapLoader::ConvertStringToColor("#0099e6")));
 
+			}
+
+			TEST_METHOD(LoadPropertiesWithoutProperties)
+			{
+				rapidxml::file<> xmlFile("TestData/Editor/Red.tsx");
+				auto doc = std::make_unique<rapidxml::xml_document<>>();
+				doc->parse<0>(xmlFile.data());
+
+				auto tilesetNode = doc->first_node(TiledMapNodeTileset);
+				std::map<std::string, System::Property> properties;
+				TiledMapLoader::LoadProperties(properties, tilesetNode);
+
+
+				Assert::AreEqual(properties.size(), size_t(0));
+			}
+
+			TEST_METHOD(LoadPropertiesWithProperties)
+			{
+				rapidxml::file<> xmlFile("TestData/Editor/Green.tsx");
+				auto doc = std::make_unique<rapidxml::xml_document<>>();
+				doc->parse<0>(xmlFile.data());
+
+				auto tilesetNode = doc->first_node(TiledMapNodeTileset);
+				std::map<std::string, System::Property> properties;
+				TiledMapLoader::LoadProperties(properties, tilesetNode, "C:/Images/");
+
+				Assert::AreEqual(properties.size(), size_t(6));
+
+				Assert::AreEqual(properties["Bool"].GetBool(), true);
+
+				Assert::IsTrue(properties["Color"].GetType() == PropertyType::COLOR);
+				Assert::IsTrue(CompareFloat::IsEqual(properties["Color"].GetColor(), TiledMapLoader::ConvertStringToColor("#ff008000")));
+
+				Assert::IsTrue(properties["File"].GetType() == PropertyType::FILE);
+				Assert::AreEqual(properties["File"].GetFile(), std::string("C:/Images/MapTest.tmx"));
+
+				Assert::IsTrue(properties["Float"].GetType() == PropertyType::FLOAT);
+				Assert::AreEqual(properties["Float"].GetFloat(), 1.2f);
+
+				Assert::IsTrue(properties["Int"].GetType() == PropertyType::INT);
+				Assert::AreEqual(properties["Int"].GetInt(), 5);
+
+				Assert::IsTrue(properties["String"].GetType() == PropertyType::STRING);
+				Assert::AreEqual(properties["String"].GetString(), std::string("Green"));
+				
+			}
+
+			TEST_METHOD(LoadTilesPropertiesWithoutProperties)
+			{
+				rapidxml::file<> xmlFile("TestData/Editor/Red.tsx");
+				auto doc = std::make_unique<rapidxml::xml_document<>>();
+				doc->parse<0>(xmlFile.data());
+
+				auto tilesetNode = doc->first_node(TiledMapNodeTileset);
+				std::map<int, std::map<std::string, System::Property>> properties;
+				TiledMapLoader::LoadTilesProperties(properties, tilesetNode, "SourceName");
+
+				Assert::AreEqual(properties.size(), size_t(0));
+			}
+
+			TEST_METHOD(LoadTilesPropertiesWithProperties)
+			{
+				rapidxml::file<> xmlFile("TestData/Editor/Green.tsx");
+				auto doc = std::make_unique<rapidxml::xml_document<>>();
+				doc->parse<0>(xmlFile.data());
+
+				auto tilesetNode = doc->first_node(TiledMapNodeTileset);
+				std::map<int, std::map<std::string, System::Property>> properties;
+				TiledMapLoader::LoadTilesProperties(properties, tilesetNode, "SourceName");
+
+				Assert::AreEqual(properties.size(), size_t(1));
+
+				Assert::AreEqual(properties.count(0), size_t(0));
+				Assert::AreEqual(properties.count(3), size_t(1));
+
+				Assert::IsTrue(properties[3]["Type"].GetType() == PropertyType::STRING);
+				Assert::AreEqual(properties[3]["Type"].GetString(), std::string("Light"));
 			}
 		};
 
